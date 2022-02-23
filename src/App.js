42229b76d78d3510419d8e02ff4c6e63659ba6b8
Richard Vado <richipoolvargas@hotmail.com>
2022-02-23 09:45:14 -0600

    map first iteration with location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Map, Marker, ZoomControl } from "pigeon-maps"
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [latitude, setLatitude] = useState(9.9347399);
+  const [longitude, setLongitude] = useState(-84.087502);
+
+
+  useEffect(() => {
+    //get from navigator the location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // separate coordenates 
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +36,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <Map height={500} defaultCenter={[latitude, longitude]} defaultZoom={11}>
+        <Marker width={50} anchor={[latitude, longitude]} color="#ff0000" />
+        <Marker width={50} anchor={[latitude, longitude-10]} color="#ff0000" />
+        <ZoomControl />
+      </Map>
+      
     </div>
   );
 }
